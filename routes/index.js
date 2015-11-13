f2556527c7005feaf92a8cb4b445cdbe73c7cc59
Kengo, MATSUDA <matsuda_kengo@goodroid.co.jp>
2015-11-13 14:18:44 +0900

    added basic item info

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,23 @@ router.get('/', function(req, res, next) {
     next(error);
   });
 });
+router.get('/item', function(req, res, next) {
+  co(function*() {
+    var options = {
+      uri: 'http://localhost:4000/item',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      json: true
+    };
+
+    var response = yield rp(options);
+    res.render('item', response);
+
+  }).catch(function(error) {
+    next(error);
+  });
+});
 router.get('/review', function(req, res, next) {
   co(function*() {
     var options = {
